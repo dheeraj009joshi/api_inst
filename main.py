b6533389dcaf3287a8554c40b61bcc9233dc1e8d
dheeraj009joshi <dlovej009@gmail.com>
2021-11-14 21:57:20 +0530

    dj

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def loc_top(username):
 
 
 if __name__ == '__main__': 
-    app.run(debug=True,port=8080)
+    app.run(debug=True)
     # app.run(debug=True,host='0.0.0.0',port=8080)#  for server 
 
 
